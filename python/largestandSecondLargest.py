f5e11e3c76cbab27bbf84836e489a75fa31c2166
rabin-nyaundi <nyaundirabin@gmail.com>
2026-02-08 00:05:03 +0300

    use one file for both largest and second largest

--- python/largestandSecondLargest.py
+++ python/largestandSecondLargest.py
@@ -1,3 +1,20 @@
+def getLargestNmber(arr: list):
+    largest = arr[0]
+    
+    if len(arr) == 0:
+        return 0
+    
+    for num in range(len(arr)):
+        if arr[num] > largest:
+            largest = arr[num]
+            
+    return largest
+            
+numbers = [1, 2, 3, 99, 4, 101, 0, 5, 6, 9, 3]           
+result = getLargestNmber(numbers)
+print(result)
+
+
 def getSecondLargest(arr: list):
     largest = arr[0]
     secondLargest = float('-inf')
